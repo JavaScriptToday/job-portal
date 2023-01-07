2cb6edac6f6e20f68d162dc7c866d48279dd0988
Goktug <cyber9unk@tutanota.de>
2023-01-07 22:48:38 +0300

    nav-bar added

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -6,10 +6,35 @@
       >
         <!-- // === Test Rules applied, if the name changes, you must modify the test - Goktug === -->
         <a
-          href="https://blog.javascripttoday.com/"
+          v-bind:href="url"
           class="itemc-center flex h-full items-center text-xl"
           >{{ company }}</a
         >
+        <nav class="ml-12 h-full">
+          <ul class="flex h-full list-none">
+            <li class="h-full">
+              <a href="" class="flex h-full items-center py-2.5">Teams</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Location</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5"
+                >Life at JavaScript Today</a
+              >
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">How we Hire</a>
+            </li>
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Students</a>
+            </li>
+
+            <li class="ml-9 h-full">
+              <a href="" class="flex h-full items-center py-2.5">Jobs</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -21,6 +46,7 @@ export default {
   data() {
     return {
       company: "JavaScript Today - Careers", // Test rules appplied. If you change the name, you must modify the test. - Goktug
+      url: "https://careers.javascripttoday.com",
     };
   },
 };
