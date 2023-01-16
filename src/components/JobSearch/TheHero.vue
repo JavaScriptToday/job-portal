e0d3b3c575aef858ba2da1816ddc9e3fb36c6e6c
Matt <faymatt96@gmail.com>
2023-01-16 00:07:10 -0500

    Add image locally

--- src/components/JobSearch/TheHero.vue
+++ src/components/JobSearch/TheHero.vue
@@ -10,7 +10,7 @@
         <div class="col-span-5 col-start-7 self-center justify-self-center">
           <img
             class="h-80 w-80 overflow-hidden rounded-full object-contain"
-            src="https://scontent.fist1-1.fna.fbcdn.net/v/t39.30808-6/291724775_181536190941602_5946967402561208652_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=8q9y3RL2QNUAX-3PCEx&_nc_ht=scontent.fist1-1.fna&oh=00_AfCDutxQoqAfNkQQQ1tcVSbT1um6DBdegV7OC9dQEHRkIA&oe=63C17313"
+            src="../../../public/js.png"
           />
         </div>
 
